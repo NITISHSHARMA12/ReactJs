9b27c34b39ab9f0d180cfcdfa27e7dd9f356e9e5
NITISHSHARMA12 <nsniteshsharma47@gmail.com>
2019-03-13 18:26:56 +0530

    create a section

--- src/index.js
+++ src/index.js
@@ -8,4 +8,23 @@ class Hello extends React.Component{
 	}
 }
 
-ReactDOM.render(<Hello/>,document.getElementById('root'))
+class World extends React.Component{
+	render(){
+		return(
+			<h2>world</h2>
+		)
+	}	
+} 
+
+class HelloWorld extends React.Component{
+	render(){
+		return(
+			<section>
+				<Hello/>
+				<World/>
+			</section>
+		)
+	}
+}
+
+ReactDOM.render(<HelloWorld/>,document.getElementById('root'))
