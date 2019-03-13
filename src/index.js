d99d82d789d7d24eb75490f4060ad1f364dbc490
NITISHSHARMA12 <nsniteshsharma47@gmail.com>
2019-03-13 18:06:43 +0530

    change component name strat with capital latter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-class hello extends React.Component{
+import ReactDOM from 'react-dom';
+class Hello extends React.Component{
 	render(){
 		return(
 			<h1>Hello</h1>
 			)
 	}
 }
+
+ReactDOM.render(<Hello/>,document.getElementById('root'))
